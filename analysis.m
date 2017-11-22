1f165645ca6a987aeb38196e785b444f321a9d02
Alicja Krzesniak <ala.krzesniak@gmail.com>
2017-11-22 13:29:02 +0000

    changes to analyse with a script analysis
    bipolar montage without other processing, makemvfile2 as a function
    DTF_analysis, analysis with very naive stationarity check

--- analysis.m
+++ analysis.m
@@ -1,42 +1,74 @@
-localisations = {'_HG', '_TG'};
-conds = {'0-2', '0-4', '0-8', '2', '4','8'};
+%files with bipolar montage exist (result of the script bipolar_montage.m)
+localisation ='_HG';
+conds = {'Coh-0-2', 'Coh-0-4', 'Coh-0-8', 'Coh-2', 'Coh-4','Coh-8'};
 subject = '348';
-fname = ['C:\Users\Alicja\Desktop\Newcastle\', subject,'\',subject,...
-    '_HG', '\coh-', '2', '.mat']; %path
+path = ['C:\Users\Alicja\Desktop\Newcastle\' subject '\' subject localisation '\'];
+cond = char(conds(6));
+montage = 0; %0 - bipolar, 1 - grand average
+mred=0; %temporal mean reduction 0/1
+subERP = 0;%subtract ERP?
+if montage==0
+    %bipolar_montage_HG(path,cond);
+    condit = [cond, '_bipolar'];
+elseif montage==1
+    %GA_montage(path,cond);
+    condit = [cond, '_GA'];
+end
+fname = [path, condit, '.mat']; 
 fs = 1000;
-%load data
-load(fname);
-nchan = size(X,1); %12 channels
+
+%% load data
+data=load(fname);
+X = data.data_out;
+nchan = size(X,1); %11 bipolar channels
 ntrls = size(X,3); %75 trials
-%downsampling
-%sig = decimate(X(1,:,1), 5);
 
-%artifact reduction
-
-%preprocessing: montage grand average (does it make sense for ECoG?)
-Xm = mean_red(X);
-%highpass filtering 
-[b,a] = butter(3, 5/(fs/2), 'high');
-sig = zeros(size(Xm));
-for tr=1:ntrls
-    for c = 1:nchan
-        sig(c,:,tr) = filtfilt(b,a, Xm(c,:,tr));
+%% downsampling
+dec = 2; %decimation factor
+sig = NaN(nchan, size(X,2)/dec, ntrls);
+fs = fs/dec;
+for ch=1:nchan
+    for tr=1:ntrls
+        sig(ch,:,tr) = decimate(X(ch,:,tr),dec);
     end
 end
+%% artifact reduction
 
-%one trial analysis
-sig_1 =squeeze(sig(:,:,1));
-
-%subtract ERP?
-sig_erp = sig_1 - mean(sig,3);
-%0-mean
-sig_0m=zeros(size(sig_1));
-for c=1:nchan
-sig_0m(c,:) = sig_erp(c,:) - mean(sig_erp(c,:));
+%% temporal mean reduction
+if mred==1
+    sig = mean_red(sig);
 end
-%normalise (std = 1)?
 
-%calculate AR coefficients (A, V)
-%transform to freq domain (->H)
-%NDTF(f) = |H(f)|^2
-%plot
+%% subtract ERP?
+if subERP==1
+    for c = 1:nchan
+        ERP = mean(sig(c,:,:),3);
+        for tr=1:ntrls    
+            sig(c,:,tr) = sig(c,:,tr) - ERP;
+        end
+    end
+end
+%% highpass filtering & random stationarity check
+t0 = 1;
+t_end = 500;
+winlen = 10;
+sum = 0;
+[b,a] = butter(3, 5/(fs/2), 'high');
+sig = zeros(size(sig));
+for tr=1:ntrls
+    for c = 1:nchan
+        sig(c,:,tr) = filtfilt(b,a, sig(c,:,tr));
+        rand_t = randi([t0 t_end - winlen],1,1);
+        sum = sum+ adftest(sig(c,rand_t:rand_t+winlen,tr));
+    end
+end
+out_fname = [path,condition, '_mont_filt_.mat'];
+save(out_fname, 'sig');
+%% DTF analysis
+fstart = 5;
+fend = 50;
+chansel = '1,2,3,4';
+winshf = 5;
+winnum = [];
+
+DTF_analysis(out_fname, nchan, ntrls, fs,fstart,fend,chansel,winlen,winshf,winnum,t0,t_end)
--- bipolar_montage_HG.m
+++ bipolar_montage_HG.m
@@ -1,25 +1,24 @@
-function X_bipolar = bipolar_montage_HG(condition)
+function bipolar_montage_HG(path, condition)
+%%condition is a string 'Coh-0-2','Coh-2' etc.
 % HG pairing
-fname = 'Coh-8';
-fs=1000;
-load([fname '.mat'],'X');
-n_HG = size(X,1); %electrodes
-bipolar_data = [];
-bipolar_labels = [];
-bipolar_type = [];
-dec = 4; %decimation factor
-X_bipolar = NaN(n_HG-1, size(X,2), size(X,3));
 
+data_in = load([path,condition '.mat'],'X');
+X = data_in.X;
+n_HG = size(X,1); %electrodes
+X_bipolar = NaN(n_HG-1, size(X,2), size(X,3));
 
 for n = 1:(n_HG-1)
    X_bipolar(n,:,:) = X(n+1,:,:) - X(n,:,:);
 %    for tr=1:size(X,3)
 %        X_bipolar(n,:,tr) = (X_bipolar(n,:,tr) - mean(X_bipolar(n,:,tr)))/std(X_bipolar(n,:,tr));  
 %    end
-
 end
-save([fname '_bipolar.mat'], 'X_bipolar');
+data_out = X_bipolar;
+save([path,condition, '_bipolar.mat'], 'data_out');
 %% TG pairing
+% bipolar_data = [];
+% bipolar_labels = [];
+% bipolar_type = [];
 % TG_ind = find(All_ChanTypes==2);
 % n_TG = numel(TG_ind);
 % gridcols = numel(Old_ChanNums)/griddims;
