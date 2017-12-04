bfe979e2a26ea0f9bc95f150f0325f9777f8d8f3
Alicja Krzesniak <ala.krzesniak@gmail.com>
2017-12-04 15:45:45 +0000

    TG analysis
    scripts expanded to do DTF analysis within TG dataset

--- main.m
+++ main.m
@@ -1,16 +1,68 @@
-cond_nr = 1;
-montage = 0; %0-bipolar, 1 - CAR
-mred = 0; subERP = 0;
-[out_fname, nchan, ntrls,fs] = preprocessing(cond_nr,montage,mred,subERP);
+%%% cond_nr: 1 to 6, conds = {'Coh-0-2','Coh-0-4','Coh-0-8','Coh-2','Coh-4','Coh-8'
+cond_nr = 6;
+mred = 0; subERP = 0; ch_del = 1; filt=0;
+subflag=1;
+loc = 1; %localisation 0 - HG, 1 - TG
+mont = 1; % .mat file exists
+%% DTF parameters
+t0 = 250;
+t_end = 950;
+winlen = 40;
+fstart = 0;
+fend = 30;
+winshf = 10;
+winnum = [];
+% chansel='6,7,9,10';
 
 %% DTF analysis
-t0 = 1;
-t_end = 1200;
-winlen = 40;
-fstart = 70;
-fend = 150;
-chansel = '1,2,3,4,5,6,7,8,9,10,11';
-winshf = 5;
-winnum = [];
+%changed amultipcolor.m!
+if loc==1
+    montage = 2; j=2;
+    descfil = 'C:\Users\Alicja\Desktop\Newcastle\348\348_TG\ChanNums-229.txt';
+    chansel = '1 , 2 , 3  ,4 , 5 , 6 , 7 , 8';%'1,9,17,25,33,40,48,56';
+else
+    j = 2;
+    montage = j; %0-bipolar, 1 - CAR, 2-common ref
+
+    if ch_del==0
+        descfil = 'C:\Users\Alicja\Desktop\Newcastle\348\348_HG\ChanNums.txt';
+        chansel = '1,2,3,4,5,6,7,8,9,10,11,12';
+        if j==0
+            chansel = '1,2,3,4,5,6,7,8,9,10,11';
+        end
+    else
+        descfil = 'C:\Users\Alicja\Desktop\Newcastle\348\348_HG\ChanNums-110.txt';
+        chansel = '1,2,3,4,5,6,7,8,9,10,11';
+        if j==0
+           chansel = '1,2,3,4,5,6,7,8,9,10';
+        end
+    end
+end
+[out_fname, nchan, ntrls,fs] = preprocessing(mont,loc,cond_nr,ch_del,montage,mred,subERP,filt);
+    
 DTF_analysis(out_fname, nchan, ntrls, fs,fstart,fend,chansel,...
-    winlen,winshf,winnum,t0,t_end)
+    descfil,winlen,winshf,winnum,t0,t_end)    
+
+montages={'bipolar','CAv','CRef'};
+
+%% Subtraction analysis
+if subflag==1
+    cond = '2'; %2/4/8
+    if loc==0
+        load([out_fname(1:end-13) '0-' cond '-110mont_DTF_datv.mat'])
+        datv_exp = load([out_fname(1:end-13) cond '-110mont_DTF_datv.mat']);
+        datv_exp=datv_exp.datv;
+        load([out_fname(1:end-13) '0-' cond '-110mont_opis.mat']);
+        load('FVL.mat');
+        smultipcolor(datv_exp-datv,0,1,FVL{3}.tt,{[char(montages(j))...
+            ' Coh-' cond ' - Coh-0-' cond] FVL{3}.sx},opis,1,0);
+    else
+        load([out_fname(1:end-13) '0-' cond '-229mont_DTF_datv.mat'])
+        datv_exp = load([out_fname(1:end-13) cond '-229mont_DTF_datv.mat']);
+        datv_exp=datv_exp.datv;
+        load([out_fname(1:end-13) '0-' cond '-229mont_opis.mat']);
+        load('FVL.mat');
+        smultipcolor(datv_exp-datv,0,1,FVL{3}.tt,{[char(montages(j))...
+            ' Coh-' cond ' - Coh-0-' cond] FVL{3}.sx},opis,1,0);
+    end
+end
--- preprocessing.m
+++ preprocessing.m
@@ -1,30 +1,78 @@
-function [out_fname, nchan, ntrls,fs]=preprocessing(cond_nr,montage,mred,subERP)
+function [out_fname, nchan, ntrls,fs]=preprocessing(mont,loc, cond_nr,ch_del,montage,mred,subERP,filt)
+%%% mont = 0; %montage done before; 0-no file, 1 - .mat file exists
+%%% loc: 0 - HG, 1 - TG
 %%% cond_nr: 1 to 6, conds = {'Coh-0-2','Coh-0-4','Coh-0-8','Coh-2','Coh-4','Coh-8'};
-%%% montage: 0 - bipolar, 1 - grand average
+%%% montage: 0 - bipolar, 1 - grand average, 2 - common reference (raw data)
 %%% mred: mean reduction 0/1
 %%% subERP: ERP subtraction 0/1
+%%% ch_110del: deleting of electrode 110 (very low std for Coh-8)
+%%% filt: flag for highpass filtering 5Hz 0/1
 
-%% montage
-%files with bipolar montage exist (result of the script bipolar_montage.m)
-localisation ='_HG';
 conds = {'Coh-0-2', 'Coh-0-4', 'Coh-0-8', 'Coh-2', 'Coh-4','Coh-8'};
 subject = '348';
+
+if loc == 0
+    localisation ='_HG';
+elseif loc == 1
+    localisation ='_TG';
+else
+    error('loc parameter must be 0 (HG) or 1 (TG)');
+end
+%% loading data
+%files with bipolar montage exist (result of the script bipolar_montage.m)
 path = ['C:\Users\Alicja\Desktop\Newcastle\' subject '\' subject localisation '\'];
 cond = char(conds(cond_nr));
 
+if mont==0
+data = load([path,cond '.mat'],'X'); 
+data_in = data.X;
+end
+%% deleting bad electrode
+%2nd in HG or 37th in TG
+if ch_del==1
+    if loc==0
+        if mont==0
+            data_in = [data_in(1,:,:);data_in(3:end,:,:)];
+        end
+        cond = [cond, '-110'];
+    else % loc==1
+        if mont==0
+            data_in = [data_in(1:36,:,:);data_in(38:end,:,:)];
+        end
+        cond = [cond, '-229'];
+    end
+end
+%% montage
+if loc==1 %only common reference for TG
+    montage=2;
+end
+
 if montage==0
-    %bipolar_montage_HG(path,cond);
+    if mont==0
+    bipolar_montage_HG(path,cond,data_in);
+    end
     condit = [cond, '_bipolar'];
 elseif montage==1
-    %GA_montage(path,cond);
+    if mont==0
+        GA_montage(path,cond, data_in);
+    end
     condit = [cond, '_GA'];
+elseif montage==2
+    if (mont==0 && ch_del==1)
+        X = data_in;
+        save([path,cond, '.mat'], 'X');
+    end
+    condit = cond;
+else
+    error('wrong montage parameter')
 end
+
 fname = [path, condit, '.mat']; 
 fs = 1000;
 
 %% load data
 data=load(fname);
-X = data.data_out;
+X = data.X;
 nchan = size(X,1); %11 bipolar channels
 ntrls = size(X,3); %75 trials
 
@@ -37,9 +85,10 @@ for ch=1:nchan
         sig(ch,:,tr) = decimate(X(ch,:,tr),dec);
     end
 end
-%% artifact reduction
 
+ 
 %% temporal mean reduction
+%done inside mmultar (DTF) package
 if mred==1
     sig = mean_red(sig);
 end
@@ -53,18 +102,34 @@ if subERP==1
         end
     end
 end
-%% highpass filtering and commented detrending
+%% highpass filtering (and commented detrending)
 %detrend(x) removes the best straight-line fit from vector x and returns it in y....
 %If x is a matrix, detrend removes the trend from each column.
-[b,a] = butter(3, 5/(fs/2), 'high');
+% we probably should detrend windowed signal, not the raw one
 sig_out = zeros(size(sig));
-for tr=1:ntrls
-    for c = 1:nchan
-        sig_out(c,:,tr) = filtfilt(b,a, sig(c,:,tr));
+if filt==1
+    [b,a] = butter(5, 5/(fs/2), 'high');
+    for tr=1:ntrls
+        for c = 1:nchan
+            sig_out(c,:,tr) = filtfilt(b,a, sig(c,:,tr));
+        end
+        %sig_out(:,:,tr) = (detrend(sig_out(:,:,tr)'))';
     end
-    %sig_out(:,:,tr) = (detrend(sig_out(:,:,tr)'))';
+    name_suffix= 'mont_filt.mat';
+else
+    name_suffix = 'mont.mat';
+    sig_out = sig;
 end
 
 %%
-out_fname = [path,condit, '_mont_filt_.mat'];
+% zm = zeros(nchan,1);
+% for i=1:nchan
+% zm(i) = mean(std(sig_out(i,:,:)));
+% end
+% disp(zm)
+% hold on
+% plot(zm)
+% title('mean std of channels')
+out_fname = [path,condit, name_suffix];
 save(out_fname, 'sig_out');
+%stationarity check may be done in licz_smtmvar.m
